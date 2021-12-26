e1a73c33868d6d7f8a6ed83b54c991a8b3b2a3b7
S Hendryx <samhendryx@gmail.com>
2021-12-26 06:14:46 -0800

    Fixed loop bugs in PlayerHand.java

--- src/main/java/PlayerHand.java
+++ src/main/java/PlayerHand.java
@@ -3,13 +3,13 @@ package main.java;
 public class PlayerHand extends Hand{
 
   public void showHand(){
-    StringBuilder showing = new StringBuilder();
-    showing.append("Player has a ");
+    StringBuilder playerHand = new StringBuilder();
+    playerHand.append("Player has a ");
     for (Card c : hand){
       if (c == hand.get(0)){
-        showing.append(" a ").append(c.toString());
+        playerHand.append(" a ").append(c.toString());
       } else {
-        showing.append(" and ").append(c.toString());
+        playerHand.append(" and ").append(c.toString());
       }
     }
   }
