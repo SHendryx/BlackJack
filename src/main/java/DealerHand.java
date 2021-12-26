cca6d0ae327dbd78f549189b361898b09ef08327
S Hendryx <samhendryx@gmail.com>
2021-12-26 06:06:19 -0800

    Fixed bugs in foreach loop in DealerHand.java

--- src/main/java/DealerHand.java
+++ src/main/java/DealerHand.java
@@ -3,11 +3,12 @@ package main.java;
 public class DealerHand extends Hand{
 
   public void showHand(){
+    StringBuilder dealerHand = new StringBuilder();
     for (Card c : hand){
       if (c != hand.get(0)){
-        StringBuilder showing = new StringBuilder();
-        showing.append("Dealer is showing one card down");
-        showing.append(" and ").append(c.toString());
+        dealerHand.append("Dealer is showing one card down");
+      } else {
+        dealerHand.append(" and ").append(c.toString());
       }
     }
   }
