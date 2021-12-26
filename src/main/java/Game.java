49c5aeae2414d2c324970a7b112075518782b084
S Hendryx <samhendryx@gmail.com>
2021-12-26 02:50:37 -0800

    Created singleton Game instance. Partially defined. Game.java

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,5 +1,25 @@
 package main.java;
 
 public class Game {
+  private static Game gameInstance = null;
 
+  private Deck gameDeck;
+  private Hand dealerHand;
+  private Hand playerHand;
+
+  private Game(){
+    // Create a new deck of cards and shuffle it.
+    Deck gameDeck = new Deck();
+    gameDeck.shuffle();
+
+    // Create two hands. One for dealer, one for player.
+
+  }
+
+  public static Game Game(){
+    if (gameInstance == null){
+      gameInstance = new Game();
+    }
+    return gameInstance;
+  }
 }
